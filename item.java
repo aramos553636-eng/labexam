9a9cf2b563766932e62e29fcb5049a572d6ca998
aramos553636-eng <a.ramos.553636@umindanao.edu.ph>
2025-09-18 21:13:05 +0800

    Create item.java

---
+++ item.java
@@ -0,0 +1,31 @@
+package alfertramos;
+
+class Item {
+    String code;
+    String name;
+    int quantity;
+
+    Item(String code, String name, int quantity) {
+        this.code = code;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public String toString() {
+        return code + " | " + name + " | " + quantity;
+    }
+}
+	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
